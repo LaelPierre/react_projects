f92640e5e4a7830bad9e0fbe814b10474157451c
Lael Aichaoui <lael.aichaoui@gmail.com>
2023-03-18 15:21:28 -0700

    fetching data

--- tabs/src/App.jsx
+++ tabs/src/App.jsx
@@ -1,6 +1,31 @@
-const url = 'https://course-api.com/react-tabs-project';
+import { useEffect, useState } from "react";
+
+const url = "https://course-api.com/react-tabs-project";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = async () => {
+    try {
+      const response = await fetch(url);
+      const newJobs = await response.json();
+      setJobs(newJobs);
+      setIsLoading(false);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (isLoading) {
+    <section className="jobs-center">
+      <div className="loading"></div>
+    </section>;
+  }
+
   return <h2>Tabs Starter</h2>;
 };
+
 export default App;
