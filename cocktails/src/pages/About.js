ad8750e81c5370b5c26014c820a4f9b63bc02870
Lael Aichaoui <lael.aichaoui@gmail.com>
2023-03-20 13:55:16 -0700

    added About and Error pages

--- cocktails/src/pages/About.js
+++ cocktails/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam eum
+        doloremque dolores praesentium temporibus cumque vel quo at, omnis
+        explicabo neque. Culpa, vero nam dignissimos a quia, excepturi rerum
+        obcaecati vitae quisquam voluptatem eos possimus provident harum modi
+        nisi reprehenderit!
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
