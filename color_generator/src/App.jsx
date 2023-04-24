b6d3c2fead270bc4ca1cb462f00411623768f155
Lael Aichaoui <lael.aichaoui@gmail.com>
2023-04-24 11:20:12 +0200

    installed values.js library

--- color_generator/src/App.jsx
+++ color_generator/src/App.jsx
@@ -1,7 +1,11 @@
 import ColorList from "./ColorList";
 import Form from "./Form";
+import Values from "values.js";
+import { useState } from "react";
 
 const App = () => {
+  const [colors, setColors] = useState([new Values("#f15025").all(10)]);
+
   return (
     <main>
       <Form />
