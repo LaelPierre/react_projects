36ad3e3f2a083350b664f953e0c14935e31ddab0
Lael Aichaoui <lael.aichaoui@gmail.com>
2023-04-27 15:28:31 +0200

    added toastify error

--- grocery_todo/src/App.jsx
+++ grocery_todo/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Form from "./Form";
 import { nanoid } from "nanoid";
 import Items from "./Items";
+import { ToastContainer, toast } from "react-toastify";
 
 // Long way
 // const getLocalStorage = () => {
@@ -33,12 +34,14 @@ const App = () => {
     const newItems = [...items, newItem];
     setItems(newItems);
     setLocalStorage(newItems);
+    toast.success("item added to the list");
   };
 
   const removeItem = (itemId) => {
     const newItems = items.filter((item) => item.id !== itemId);
     setItems(newItems);
     setLocalStorage(newItems);
+    toast.success("item deleted");
   };
 
   const editItem = (itemId) => {
@@ -55,6 +58,7 @@ const App = () => {
 
   return (
     <section className="section-center">
+      <ToastContainer position="top-center" />
       <Form addItem={addItem} />
       <Items items={items} removeItem={removeItem} editItem={editItem} />
     </section>
