55a9c350e2d4441f41d465d0a7f58bf0b0bd92c5
Lael Aichaoui <lael.aichaoui@gmail.com>
2023-04-28 15:35:09 +0200

    added css for the big screen

--- navbar/src/NavBar.jsx
+++ navbar/src/NavBar.jsx
@@ -45,6 +45,18 @@ const NavBar = () => {
             })}
           </ul>
         </div>
+
+        {/* social link */}
+        <ul className="social-icons">
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </nav>
   );
