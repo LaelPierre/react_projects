9b620380b7eff716d365b463ea283ff2cbbd57ca
Lael Aichaoui <lael.aichaoui@gmail.com>
2023-05-04 11:03:06 +0200

    added css for modal

--- sidebar_modal/src/Modal.jsx
+++ sidebar_modal/src/Modal.jsx
@@ -6,7 +6,7 @@ const Modal = () => {
   const { isModalOpen, closeModal } = useGlobalContext();
 
   return (
-    <div className="modal-overlay">
+    <div className={isModalOpen ? "modal-overlay show-modal" : "modal-overlay"}>
       <div className="modal-container">
         <h3>modal content</h3>
         <button className="close-modal-btn" onClick={closeModal}>
