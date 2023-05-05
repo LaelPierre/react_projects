eb3cf837eb98e3040333d03d9940e70fef4b0d5c
Lael Aichaoui <lael.aichaoui@gmail.com>
2023-05-05 11:04:30 +0200

    added hero component

--- strapi_menu/src/Hero.jsx
+++ strapi_menu/src/Hero.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Hero = () => {
-  return <h2>Hero</h2>;
+  return (
+    <div className="hero container">
+      <div className="hero-center">
+        <h1>
+          manage any content <br /> anywhere
+        </h1>
+        <p>
+          Strapi is the leading open-source headless CMS. It’s 100% JavaScript
+          and fully customizable.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
