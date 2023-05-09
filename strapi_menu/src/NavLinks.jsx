8cfb675a621a18e5f737c4f40b84c84c47d13db2
Lael Aichaoui <lael.aichaoui@gmail.com>
2023-05-09 10:45:34 +0200

    added overring context

--- strapi_menu/src/NavLinks.jsx
+++ strapi_menu/src/NavLinks.jsx
@@ -1,14 +1,21 @@
 import React from "react";
+import { useGlobalContext } from "./Context";
 import sublinks from "./data";
 
 const NavLinks = () => {
+  const { setPageId } = useGlobalContext();
+
   return (
     <div className="nav-links">
       {sublinks.map((item) => {
         const { pageId, page } = item;
 
         return (
-          <button key={pageId} className="nav-link">
+          <button
+            key={pageId}
+            className="nav-link"
+            onMouseEnter={() => setPageId(pageId)}
+          >
             {page}
           </button>
         );
