35f45bd7e51ef3f2b68fe91e77de77f25b648109
Lael Aichaoui <lael.aichaoui@gmail.com>
2023-05-11 22:20:25 +0800

    setup usequery

--- unsplash_images/src/Gallery.jsx
+++ unsplash_images/src/Gallery.jsx
@@ -1,6 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import React from "react";
 
+const url =
+  "https://api.unsplash.com/search/photos?client_id=JvIoAkzLNkmoHSROFHNI9T96zUfsL_RduCccnmX8zgA&query=office";
+
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url);
+      return result.data;
+    },
+  });
+
   return <h2>Gallery</h2>;
 };
 
