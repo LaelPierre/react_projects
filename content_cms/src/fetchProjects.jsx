6c99f8fc4206dfa76e9921b9b42313ecfe95a479
Lael Aichaoui <lael.aichaoui@gmail.com>
2023-05-12 17:27:00 +0800

    created env file

--- content_cms/src/fetchProjects.jsx
+++ content_cms/src/fetchProjects.jsx
@@ -4,7 +4,7 @@ import { createClient } from "contentful";
 const client = createClient({
   space: "4octp27xqpo3",
   environment: "master",
-  accessToken: "CvrJO75v-oztGBWv_TYy_Sl3kFlSSn71NIoeH4Ql0AU",
+  accessToken: import.meta.env.VITE_API_KEY,
 });
 
 export const useFetchProjects = () => {
