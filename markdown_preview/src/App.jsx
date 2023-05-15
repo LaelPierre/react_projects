4a342b218a3838b9be3204d7478b3f678ae8c82f
Lael Aichaoui <lael.aichaoui@gmail.com>
2023-05-15 11:17:33 +0200

    added markdown functionnality

--- markdown_preview/src/App.jsx
+++ markdown_preview/src/App.jsx
@@ -1,4 +1,22 @@
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+
 const App = () => {
-  return <h1>Unsplash Images Starter</h1>;
+  const [markdown, setMarkdown] = useState("## markdown preview");
+
+  return (
+    <main>
+      <section className="markdown">
+        <textarea
+          className="input"
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        ></textarea>
+        <article className="result">
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
+  );
 };
 export default App;
