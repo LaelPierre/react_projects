1686d38661db819e38bc8f7c2af3a89213fc148f
Lael Aichaoui <lael.aichaoui@gmail.com>
2023-11-11 17:26:10 -0800

    forgot last commit

--- pagination/src/App.jsx
+++ pagination/src/App.jsx
@@ -1,4 +1,9 @@
+import { useFetch } from "./useFetch";
+
 const App = () => {
+  const { loading, data } = useFetch();
+  console.log(data);
+
   return <h1>Pagination</h1>;
 };
 export default App;
